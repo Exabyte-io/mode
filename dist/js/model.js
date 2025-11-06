befd313632f114b36b5717b28589a63a9b5aa797
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-06 11:06:44 -0800

    update: set method in constructor

--- dist/js/model.js
+++ dist/js/model.js
@@ -14,10 +14,13 @@ const tree_1 = require("./tree");
 const EMPTY_BRANCH = { methods: {} };
 class Model extends entity_1.InMemoryEntity {
     constructor(config) {
-        const { application, ...entityConfig } = config;
+        const { application, method = method_1.Method.defaultConfig, ...entityConfig } = config;
         super(entityConfig);
         this._application = application;
         this._MethodFactory = factory_1.MethodFactory;
+        if (method) {
+            this.setProp("method", method);
+        }
     }
     setSubtype(subtype) {
         this.setProp("subtype", subtype);
@@ -65,7 +68,7 @@ class Model extends entity_1.InMemoryEntity {
     }
     get Method() {
         if (!this._method) {
-            this._method = this._MethodFactory.create(this.method || this.defaultMethodConfig);
+            this._method = this._MethodFactory.create(this.method);
         }
         return this._method;
     }
