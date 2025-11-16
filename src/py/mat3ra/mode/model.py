b17fb5beb1220c933321531b8ea93382ec4d1e81
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-16 14:51:47 -0800

    chore: fix lint

--- src/py/mat3ra/mode/model.py
+++ src/py/mat3ra/mode/model.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, Optional
 
 from mat3ra.code.entity import InMemoryEntityPydantic
-from mat3ra.esse.models.core.primitive.slugified_entry import SlugifiedEntry
 from mat3ra.esse.models.model import BaseModel1
 from pydantic import Field
 
